0286a5842de1773b2d9ed12c9268d06a3542bd76
Jiawen He <hanghur@gmail.com>
2022-02-17 10:30:58 +1100

    Progress (fixed 2d pointers)

--- anu_inversion_course/rjmcmc.py
+++ anu_inversion_course/rjmcmc.py
@@ -3,19 +3,23 @@ from anu_inversion_course._rjmcmc import (
     dataset1d_load_known,
     dataset1d_load_fixed,
     dataset1d_create,
+    resultset1d_t,
+    py_resultset1d_get_histogram,
+    # resultset1dfm_t,
 )
 
 import collections
 import numpy as np
 
 
-class dataset1d():
+class dataset1d:
     def __init__(self, filename=None, n=None, x=None, y=None):
         if filename is not None and n is None:
             self.d = dataset1d_load_known(filename)
         elif filename is not None and n is not None:
             self.d = dataset1d_load_fixed(filename, n)
         elif x is not None and y is not None and n is not None:
+            # ####### INPUT VALIDATION #######
             if not check_list_like(x) or not check_list_like(y) or not check_list_like(n):
                 raise ValueError("Parameter must be a list of floating point values")
             x = np.asanyarray(x, dtype=np.float64)
@@ -23,8 +27,165 @@ class dataset1d():
             n = np.asanyarray(n, dtype=np.float64)
             if x.shape[0] != y.shape[0] or x.shape[0] != n.shape[0]:
                 raise ValueError("Parameters must have the same length")
+            if np.any(n<0):
+                raise ValueError("All values in the n array must be greater than zero")
+            # ####### END VALIDATION #######
             self.d = dataset1d_create(x.shape[0])
-            self.x = x
+            for i in range(x.shape[0]):
+                self.d.points[i].x = x[i]
+                self.d.points[i].y = y[i]
+                self.d.points[i].n = n[i]
+            self.d.xmin = np.min(x)
+            self.d.xmax = np.max(x)
+            self.d.ymin = np.min(y)
+            self.d.ymax = np.max(y)
+        else:
+            raise ValueError("Please provide either file name or x,y,n when initialising dataset1d")
+
+    def set_xrange(self, xmin, xmax):
+        self.d.xmin = xmin
+        self.d.xmax = xmax
+
+    def get_xmin(self):
+        return self.d.xmin
+
+    def get_xmax(self):
+        return self.d.xmax
+
+    def set_yrange(self, ymin, ymax):
+        self.d.ymin = ymin
+        self.d.ymax = ymax
+
+    def get_ymin(self):
+        return self.d.ymin
+
+    def get_ymax(self):
+        return self.d.ymax
+
+    def set_lambda_std(self, std):
+        self.d.lambdastd = std
+    
+    def get_lambda_std(self):
+        return self.d.lambdastd
+
+    def set_lambda_range(self, lambdamin, lambdamax):
+        self.d.lambdamin = lambdamin
+        self.d.lambdamax = lambdamax
+
+    def get_lambda_min(self):
+        return self.d.lambdamin
+
+    def get_lambda_max(self):
+        return self.d.lambdamax
+
+class resultset1d:
+    def __init__(self, resultset1d_t):
+        self.r = resultset1d_t
+
+    def proposed(self):
+        return self.r.propose
+            
+    def acceptance(self):
+        return self.r.accept
+
+    def partitions(self):
+        return self.r.partitions
+
+    def order_histogram(self):
+        return self.r.order
+
+    def partition_histogram(self):
+        return self.r.partitions
+
+    def partition_location_histogram(self):
+        return self.r.partition_x_hist
+
+    def x(self):
+        n_xsamples = self.r.xsamples
+        xmin = self.r.xmin
+        xmax = self.r.xmax
+        x_vector = [xmin+(xmax-xmin)*(i+0.5)/n_xsamples for i in range(n_xsamples)]
+        return x_vector
+
+    def y(self):
+        n_ysamples = self.r.ysamples
+        ymin = self.r.ymin
+        ymax = self.r.ymax
+        y_vector = [ymin+(ymax-ymin)*(i+0.5)/n_ysamples for i in range(n_ysamples)]
+        return y_vector
+
+    def mean(self):
+        return self.r.mean
+
+    def median(self):
+        return self.r.median
+
+    def mode(self):
+        return self.r.mode
+
+    def credible_min(self):
+        return self.r.conf_min
+
+    def credible_max(self):
+        return self.r.conf_max
+
+    def misfit(self):
+        return self.r.misfit
+
+    def lambda_history(self):
+        return self.r.getattr("lambda")
+
+    def histogram(self):
+        return py_resultset1d_get_histogram(self.r)
+
+
+class resultset1dfm:
+    def __init__(self, resultset1dfm_t):
+        self.r = resultset1dfm_t
+
+    def proposed(self):
+        return self.r.propose
+            
+    def acceptance(self):
+        return self.r.accept
+
+    def partitions(self):
+        return self.r.partitions
+
+    def partition_histogram(self):
+        return self.r.partitions
+
+    def partition_location_histogram(self):
+        return self.r.partition_x_hist
+
+    def x(self):
+        return self.r.xsamples
+
+    def y(self):
+        return self.r.ysamples
+
+    def mean(self):
+        return self.r.mean
+
+    def median(self):
+        return self.r.median
+
+    def mode(self):
+        return self.r.mode
+
+    def credible_min(self):
+        return self.r.conf_min
+
+    def credible_max(self):
+        return self.r.conf_max
+
+    def misfit(self):
+        return self.r.misfit 
+    
+    def global_parameter(self, gi):
+        return self.r.getattr("global")[gi]
+
+
 
 def check_list_like(x):
     return isinstance(x, collections.abc.Iterable) and not isinstance(x, (str, bytes))
--- anu_inversion_course/rjmcmc/_rjmcmc.cpp
+++ anu_inversion_course/rjmcmc/_rjmcmc.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <Python.h>
 
 #include <iostream>
 
@@ -10,6 +11,13 @@ void hello() {
 
 extern "C" {
     #include "python/swig/rjmcmc_helper.h"
+}
+
+PyObject *py_resultset1d_get_histogram(resultset1d_t *r) {
+    const int **hist = resultset1d_get_histogram(r);
+    PyObject *res;
+    res = pyrjmcmc_make_int_list_2d(hist, r->xsamples, r->ysamples);
+    return res;
 }
 
 
@@ -23,9 +31,10 @@ PYBIND11_MODULE(_rjmcmc, m) {
     m.doc() = "Reversible Jump McMC";
     m.def("hello", &hello, "Prints \"Hello, world!\"");
     m.def("rjmcmc_seed", &rjmcmc_seed, "Set random seed given an integer");
+    
+    // ------------ BEGIN dataset1d_t ---------------------------------
     py::class_<dataset1d_t>(m, "dataset1d_t")
         .def(py::init<>())
-        // .def(py::init<const std::string &>())
         .def_readwrite("xmin", &dataset1d_t::xmin)
         .def_readwrite("xmax", &dataset1d_t::xmax)
         .def_readwrite("ymin", &dataset1d_t::ymin)
@@ -41,5 +50,82 @@ PYBIND11_MODULE(_rjmcmc, m) {
     m.def("dataset1d_load_known", &dataset1d_load_known, "Loads a 1D dataset from given file name");
     m.def("dataset1d_load_fixed", &dataset1d_load_fixed, "Loads a 1D dataset from given file name, and applies a fixed noise level to each data point");
     m.def("dataset1d_create", &dataset1d_create, "Create a new empty dataset");
-    // m.def("regression_single1d", &regression_single1d, "Performs single");
+   
+    // ------------ BEGIN resultset1d_t ---------------------------------
+    py::class_<resultset1d_t>(m, "resultset1d_t")
+        .def(py::init<>())
+        .def_readwrite("results", &resultset1d_t::results)
+        .def_readwrite("burnin", &resultset1d_t::burnin)
+        .def_readwrite("total", &resultset1d_t::total)
+        .def_readwrite("xsamples", &resultset1d_t::xsamples)
+        .def_readwrite("ysamples", &resultset1d_t::ysamples)
+        .def_readwrite("nprocesses", &resultset1d_t::nprocesses)
+        .def_readwrite("maxpartitions", &resultset1d_t::maxpartitions)
+        .def_readwrite("maxorder", &resultset1d_t::maxorder)
+        .def_readwrite("xmin", &resultset1d_t::xmin)
+        .def_readwrite("xmax", &resultset1d_t::xmax)
+        .def_readwrite("ymin", &resultset1d_t::ymin)
+        .def_readwrite("ymax", &resultset1d_t::ymax)
+        .def_readwrite("gradmin", &resultset1d_t::gradmin)
+        .def_readwrite("gradmax", &resultset1d_t::gradmax)
+        .def_readwrite("propose", &resultset1d_t::propose)
+        .def_readwrite("accept", &resultset1d_t::accept)
+        .def_readwrite("misfit", &resultset1d_t::misfit)
+        .def_readwrite("lambda", &resultset1d_t::lambda)
+        .def_readwrite("order", &resultset1d_t::order)
+        .def_readwrite("partitions", &resultset1d_t::partitions)
+        .def_readwrite("partition_x_hist", &resultset1d_t::partition_x_hist)
+        .def_readwrite("mean", &resultset1d_t::mean)
+        // .def_readwrite("hist", &resultset1d_t::hist)
+        .def_readwrite("mode", &resultset1d_t::mode)
+        .def_readwrite("median", &resultset1d_t::median)
+        .def_readwrite("conf_interval", &resultset1d_t::conf_interval)
+        .def_readwrite("conf_min", &resultset1d_t::conf_min)
+        .def_readwrite("conf_max", &resultset1d_t::conf_max)
+        .def_readwrite("gradient", &resultset1d_t::gradient)
+        // .def_readwrite("gradient_hist", &resultset1d_t::gradient_hist)
+        .def_readwrite("gradient_conf_min", &resultset1d_t::gradient_conf_min)
+        .def_readwrite("gradient_conf_max", &resultset1d_t::gradient_conf_max);
+    m.def("py_resultset1d_get_histogram", &py_resultset1d_get_histogram);
+    
+    // // ------------ BEGIN resultset1dfm_t ---------------------------------
+    // py::class_<resultset1dfm_t>(m, "resultset1dfm_t")
+    //     .def(py::init<>())
+    //     .def_readwrite("results", &resultset1dfm_t::results)
+    //     .def_readwrite("burnin", &resultset1dfm_t::burnin)
+    //     .def_readwrite("total", &resultset1dfm_t::total)
+    //     .def_readwrite("xsamples", &resultset1dfm_t::xsamples)
+    //     .def_readwrite("ysamples", &resultset1dfm_t::ysamples)
+    //     .def_readwrite("nglobalparameters", &resultset1dfm_t::nglobalparameters)
+    //     .def_readwrite("global_parameters", &resultset1dfm_t::global_parameters)
+    //     .def_readwrite("nlocalparameters", &resultset1dfm_t::nlocalparameters)
+    //     .def_readwrite("local_parameters", &resultset1dfm_t::local_parameters)
+    //     .def_readwrite("maxpartitions", &resultset1dfm_t::maxpartitions)
+    //     .def_readwrite("xmin", &resultset1dfm_t::xmin)
+    //     .def_readwrite("xmax", &resultset1dfm_t::xmax)
+    //     .def_readwrite("nprocesses", &resultset1dfm_t::nprocesses)
+    //     .def_readwrite("propose", &resultset1dfm_t::propose)
+    //     .def_readwrite("accept", &resultset1dfm_t::accept)
+    //     .def_readwrite("propose_local", &resultset1dfm_t::propose_local)
+    //     .def_readwrite("accept_local", &resultset1dfm_t::accept_local)
+    //     .def_readwrite("misfit", &resultset1dfm_t::misfit)
+    //     .def_readwrite("partitions", &resultset1dfm_t::partitions)
+    //     .def_readwrite("partition_x_hist", &resultset1dfm_t::partition_x_hist)
+    //     .def_readwrite("global", &resultset1dfm_t::global)
+    //     .def_readwrite("local_mean", &resultset1dfm_t::local_mean)
+    //     .def_readwrite("nhierarchical", &resultset1dfm_t::nhierarchical)
+    //     .def_readwrite("hierarchical", &resultset1dfm_t::hierarchical)
+    //     .def_readwrite("histogram", &resultset1dfm_t::histogram)
+    //     .def_readwrite("local_median", &resultset1dfm_t::local_median)
+    //     .def_readwrite("local_mode", &resultset1dfm_t::local_mode)
+    //     .def_readwrite("credible_interval", &resultset1dfm_t::credible_interval)
+    //     .def_readwrite("local_cred_min", &resultset1dfm_t::local_cred_min)
+    //     .def_readwrite("local_cred_max", &resultset1dfm_t::local_cred_max);
+    
+    // ------------ BEGIN single partition ---------------------------------
+    m.def("regression_single1d", &regression_single1d);
+    m.def("regression_single1d_sampled", &regression_single1d_sampled);
+
+    // m.def("add", &add, "A function which adds two numbers",
+    //   py::arg("i") = 1, py::arg("j") = 2); 
 }
